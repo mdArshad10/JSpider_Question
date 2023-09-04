f7faa1241fa297dc9bfd170a55dd9a345bbe57b6
mdArshad10 <mdarshd246@gmail.com>
2023-09-04 14:37:29 +0530

    adding the question and

--- index.js
+++ index.js
@@ -3,13 +3,26 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res
-    .status(400)
-    .json({
-      message:
-        "hi this is the my first application which is deployed at github page",
-      status: 201,
-    });
+  const questions = [
+    "what is java?",
+    "what is varible",
+    "Difference between Global and local variable",
+    "what is static",
+    "What is non-static",
+    "what is Block",
+    "why static has one copy",
+    "why non-static has multiply copy",
+    "what is object",
+    "what is Reference Variable?"
+  ];
+  console.log(questions.length);
+
+  let position = Math.floor(Math.random()*10);
+  let question = questions[position];
+  res.status(400).json({
+    question,
+    status: 201,
+  });
 });
 
 app.listen(5000, () => {
